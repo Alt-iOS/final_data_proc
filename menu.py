d1e25ccb54ca0cc86f3cfb2a1560de61dbc65934
victor <vjbsh@hotmail.com>
2024-05-14 16:01:10 -0600

    menu with option for each and infinite loop

--- menu.py
+++ menu.py
@@ -1,43 +1,55 @@
-from unittest import case
+def main():
+    while True:
+        try:
+            option = int(input("Bienvenido al programa final de Procesamiento de datos\n"
+                               "Hecho por Víctor Batarse y Sofia Pérez\n"
+                               "Para comenzar seleccione una opción\n"
+                               "1. Regresión Lineal\n"
+                               "2. Regresión Logística\n"
+                               "3. Modelo Epidemiológico\n"
+                               "4. Modelo de Inversión\n"
+                               "5. Red Neuronal - reconocimiento de números\n"
+                               "6. Red Neuronal Tienda de Ropa\n"
+                               "7. AGS Asesor de Inversión\n"
+                               "8. Colonia de Hormigas\n"
+                               "9. Enjambre de Partículas\n"
+                               "10. Sistemas inmunes artificiales\n"
+                               "11. Salir\n"))
+        except ValueError:
+            print("Ups, no se ingresó un número. Intente de nuevo.")
+            continue
 
-print("Bienvenido al programa final de Procesamiento de datos\n"
-      "Hecho por Víctor Batarse y Sofia Pérez\n"
-      "Para comenzar seleccione una opción\n"
-      "1. Regresión Lineal\n"
-      "2. Regresión Logística\n"
-      "3. Modelo Epidemiológico\n"
-      "4. Modelo de Inversión\n"
-      "5. Red Neuronal - reconocimiento de números\n"
-      "6. Red Neuronal Tienda de Ropa\n"
-      "7. AGS Asesor de Inversión\n"
-      "8. Colonia de Hormigas\n"
-      "9. Enjambre de Partículas\n"
-      "10. Sistemas inmunes artificiales\n"
-      "11. Salir")
+        process_option(option)
 
-option = 0
 
-match option:
-    case 1:
-        print("imp[lementacion algoritmo ")
-    case 2:
-        print("imp[lementacion algoritmo ")
-    case 3:
-        print("imp[lementacion algoritmo ")
-    case 4:
-        print("imp[lementacion algoritmo ")
-    case 5:
-        print("imp[lementacion algoritmo ")
-    case 6:
-        print("imp[lementacion algoritmo ")
-    case 7:
-        print("imp[lementacion algoritmo ")
-    case 8:
-        print("imp[lementacion algoritmo ")
-    case 9:
-        print("imp[lementacion algoritmo ")
-    case 10:
-        print("imp[lementacion algoritmo ")
-    case 11:
-        exit(0)
+def process_option(option):
+    match option:
+        case 1:
+            print("Implementación algoritmo Regresión Lineal")
+        case 2:
+            print("Implementación algoritmo Regresión Logística")
+        case 3:
+            print("Implementación Modelo Epidemiológico")
+        case 4:
+            print("Implementación Modelo de Inversión")
+        case 5:
+            print("Implementación Red Neuronal - reconocimiento de números")
+        case 6:
+            print("Implementación Red Neuronal Tienda de Ropa")
+        case 7:
+            print("Implementación AGS Asesor de Inversión")
+        case 8:
+            print("Implementación Colonia de Hormigas")
+        case 9:
+            print("Implementación Enjambre de Partículas")
+        case 10:
+            print("Implementación Sistemas inmunes artificiales")
+        case 11:
+            print("Gracias por usar el programa. ¡Hasta pronto!")
+            exit(0)
+        case _:
+            print("Opción no disponible, tiene que ser un número entre 1-11")
 
+
+if __name__ == "__main__":
+    main()
