43a9a4a23073244f658eee5cd2506228e7e7e523
victor <vjbsh@hotmail.com>
2024-05-14 15:34:32 -0600

    first commit

---
+++ menu.py
@@ -0,0 +1,15 @@
+print("Bienvenido al programa final de Procesamiento de datos\n"
+      "Hecho por Víctor Batarse y Sofia Pérez\n"
+      "Para comenzar seleccione una opción\n"
+      "1. Regresión Lineal\n"
+      "2. Regresión Logística\n"
+      "3. Modelo Epidemiológico\n"
+      "4. Modelo de Inversión\n"
+      "5. Red Neuronal - reconocimiento de números\n"
+      "6. Red Neuronal Tienda de Ropa\n"
+      "7. AGS Asesor de Inversión\n"
+      "8. Colonia de Hormigas\n"
+      "9. Enjambre de Partículas\n"
+      "10. Sistemas inmunes artificiales\n"
+      "0. Salir")
+
