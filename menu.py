8ee67ac6467a413e667ab465944100004e864ab1
victor <vjbsh@hotmail.com>
2024-05-14 15:47:16 -0600

    basic menu

--- menu.py
+++ menu.py
@@ -1,3 +1,5 @@
+from unittest import case
+
 print("Bienvenido al programa final de Procesamiento de datos\n"
       "Hecho por Víctor Batarse y Sofia Pérez\n"
       "Para comenzar seleccione una opción\n"
@@ -11,5 +13,31 @@ print("Bienvenido al programa final de Procesamiento de datos\n"
       "8. Colonia de Hormigas\n"
       "9. Enjambre de Partículas\n"
       "10. Sistemas inmunes artificiales\n"
-      "0. Salir")
+      "11. Salir")
+
+option = 0
+
+match option:
+    case 1:
+        print("imp[lementacion algoritmo ")
+    case 2:
+        print("imp[lementacion algoritmo ")
+    case 3:
+        print("imp[lementacion algoritmo ")
+    case 4:
+        print("imp[lementacion algoritmo ")
+    case 5:
+        print("imp[lementacion algoritmo ")
+    case 6:
+        print("imp[lementacion algoritmo ")
+    case 7:
+        print("imp[lementacion algoritmo ")
+    case 8:
+        print("imp[lementacion algoritmo ")
+    case 9:
+        print("imp[lementacion algoritmo ")
+    case 10:
+        print("imp[lementacion algoritmo ")
+    case 11:
+        exit(0)
 
